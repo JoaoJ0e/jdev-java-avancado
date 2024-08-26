9e6268fa85c1a56ed1106801cc33e5e74705dcb1
unknown <joao.griebner@gmail.com>
2024-08-26 11:51:00 -0300

    Upd exercicios - completos

--- src/main/java/jv/triersistemas/primeiro_projeto/controller/TarefaController.java
+++ src/main/java/jv/triersistemas/primeiro_projeto/controller/TarefaController.java
@@ -3,8 +3,10 @@ package jv.triersistemas.primeiro_projeto.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,17 +27,37 @@ public class TarefaController {
 	}
 
 	@GetMapping("/get-by-id")
-	public Tarefa getById(@RequestBody int id) {
-		return tarefas.get(id);
+	public Tarefa getById(@RequestBody Integer id) {
+		return tarefas.stream()
+		.filter(tarefa -> id == tarefa.getId())
+		.findAny()
+		.orElse(null);
 	}
 
-	@PostMapping
+	@PostMapping("/cadastra-tarefa")
 	public void cadastraTarefa(@RequestBody Tarefa tarefa) {
 		contador += 1;
 		tarefa.defineId(contador);
 		tarefas.add(tarefa);
 	}
+	
+	@PutMapping("/edita-tarefa")
+	public void editaTarefa(@RequestBody Tarefa tarefa) {
+		tarefas.set((tarefas.indexOf(tarefas.stream()
+				.filter(item -> tarefa.getId() == item.getId())
+				.findAny()
+				.orElse(null))), tarefa);
+	}
 
+	@DeleteMapping("/deleta-tarefa")
+	public void deletaTarefa(@RequestBody Integer id) {
+		tarefas.remove(
+				tarefas.indexOf
+				(tarefas.stream()
+				.filter(item -> id == item.getId())
+				.findAny()
+				.orElse(null)));
+	}
 }
 
 @NoArgsConstructor
